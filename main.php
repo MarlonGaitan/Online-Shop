5da559c01760bf5d01e6f6010f9ace3912341bda
Marlon Gaitan Laptop <marlongb2002@gmail.com>
2023-04-13 00:12:49 -0500

    Carrousel updated, is functional

--- main.php
+++ main.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<!-- ===== Link Swiper's CSS ===== -->
+		<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
 		<script src="js/ShopProjectjs.js"></script>
 		<link rel="stylesheet" href="css/ShopProjectCSS.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,56 +18,85 @@
 				</div>
 		</div>
 		<!-- Slideshow container -->
-		<div class="slideshow-container">
-
+		<div class="swiper mySwiper slideshow-container">
+			<div class="swiper-wrapper slideshow-content">
 		  <!-- Full-width images with number and caption text -->
-		  <div class="mySlides fade">
-			<div class="numbertext">1 / 3</div>
-				<img src="img/cart.png">
-			<div class="text">Caption Text</div>
-		  </div>
+		  		<div class="swiper-slide mySlides fade">
+					<div class="mySlides-content">
+						<div class="image">
+							<img src="img/cart.png">
+						</div>				
+						<div class="text">
+							<span>Electronics</span>	
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide mySlides fade">
+					<div class="mySlides-content">
+						<div class="image">
+							<img src="img/cart.png">
+						</div>				
+						<div class="text">
+							<span>Literature</span>	
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide mySlides fade">
+					<div class="mySlides-content">
+						<div class="image">
+							<img src="img/cart.png">
+						</div>				
+						<div class="text">
+							<span>Clothing</span>	
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide mySlides fade">
+					<div class="mySlides-content">
+						<div class="image">
+							<img src="img/cart.png">
+						</div>				
+						<div class="text">
+							<span>Outdoors</span>	
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide mySlides fade">
+					<div class="mySlides-content">
+						<div class="image">
+							<img src="img/cart.png">
+						</div>				
+						<div class="text">
+							<span>Sports</span>	
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide mySlides fade">
+					<div class="mySlides-content">
+						<div class="image">
+							<img src="img/cart.png">
+						</div>				
+						<div class="text">
+							<span>Pet Supplies</span>	
+						</div>
+					</div>
+				</div>
 
-		  <div class="mySlides fade">
-			<div class="numbertext">2 / 3</div>
-				<img src="img/logo.png">
-			<div class="text">Caption Two</div>
+				<!-- The dots/circles -->
+				<!-- <div class = "three-dots" style="text-align:center">
+					<span class="dot" onclick="currentSlide(1)"></span>
+					<span class="dot" onclick="currentSlide(2)"></span>
+					<span class="dot" onclick="currentSlide(3)"></span>
+				</div> -->
 		  </div>
-
-		  <div class="mySlides fade">
-			<div class="numbertext">3 / 3</div>
-				<img src="img/userIcon.png">
-			<div class="text">Caption Three</div>
-		  </div>
-		  <div class="mySlides fade">
-			<div class="numbertext">1 / 3</div>
-				<img src="img/cart.png">
-			<div class="text">Caption Text</div>
-		  </div>
-
-		  <div class="mySlides fade">
-			<div class="numbertext">2 / 3</div>
-				<img src="img/logo.png">
-			<div class="text">Caption Two</div>
-		  </div>
-
-		  <div class="mySlides fade">
-			<div class="numbertext">3 / 3</div>
-				<img src="img/userIcon.png">
-			<div class="text">Caption Three</div>
-		  </div>
-
-		  <!-- Next and previous buttons -->
-		  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-		  <a class="next" onclick="plusSlides(1)">&#10095;</a>
 		</div>
-		<br>
+		<div class="swiper-button-next"></div>
+		<div class="swiper-button-prev"></div>
+		<div class="swiper-pagination"></div>
+		<!-- Swiper JS -->
+		<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
-		<!-- The dots/circles -->
-			<div style="text-align:center">
-			<span class="dot" onclick="currentSlide(1)"></span>
-			<span class="dot" onclick="currentSlide(2)"></span>
-			<span class="dot" onclick="currentSlide(3)"></span>
-		</div>
+		
 		
 		<?php
 			include ("db_connection.php");
@@ -101,7 +132,23 @@
 			</div>		  
 		</div>
 		</div>
-		
+		<script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  </script>
 		
 	</body>
 </html>
