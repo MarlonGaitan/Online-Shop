4cb9eb69a0a9a16cc321d0424fb5ff504b3a9660
keysh18 <114112736+keysh18@users.noreply.github.com>
2023-04-13 11:11:45 -0500

    Update signup_process.php

--- signup_process.php
+++ signup_process.php
@@ -4,9 +4,11 @@
 	$username=($_POST['username']);
 	$pass=($_POST['password']);
 	$dob=($_POST['dob']);
+	echo $email;
+	echo $dob;
 ?>
 
 <?php
-    $sql="INSERT INTO users_tab(username, email, date_of_birth, password) VALUES ('$email','$username','$pass','$dob')";
+    $sql="INSERT INTO `users_tab`(`sid`, `username`, `email`, `date_of_birth`, `password`, `premium`) VALUES (0, '$username','$email','$dob','$pass', 0)";
 	$result=$connect->query($sql);
 ?>
