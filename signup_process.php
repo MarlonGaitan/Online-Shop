498703499c835476057828d6e41ac93c991a42ab
Marlon Gaitan Laptop <marlongb2002@gmail.com>
2023-04-15 17:58:51 -0500

    database connection solved

--- signup_process.php
+++ signup_process.php
@@ -9,12 +9,6 @@
 ?>
 
 <?php
-<<<<<<< HEAD
-    $sql="INSERT INTO users_tab(username, email, date_of_birth, password) VALUES ('$email','$username','$pass','$dob')";
-	$result=$conn->query($sql);
-?>
-=======
     $sql="INSERT INTO `users_tab`(`sid`, `username`, `email`, `date_of_birth`, `password`, `premium`) VALUES (0, '$username','$email','$dob','$pass', 0)";
 	$result=$connect->query($sql);
 ?>
->>>>>>> 4cb9eb69a0a9a16cc321d0424fb5ff504b3a9660
