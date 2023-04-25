f9f140c092f233beb332b1840a22fe41de7b2bf3
keysh18 <114112736+keysh18@users.noreply.github.com>
2023-04-25 00:12:34 -0500

    Add files via upload

---
+++ premium.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+	include("db_connection.php");
+
+?>
+<html>
+	<head>
+		<!-- ===== Link Swiper's CSS ===== -->
+		<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+		<link rel="stylesheet" href="css/ShopProjectCSS.css">
+		<link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon.ico">
+		<link rel="manifest" href="favicon_io/site.webmanifest">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body bgcolor="#262626">
+		<?php
+			include("banner.php");
+		?>
+		<h1 style= "color:white">You have successfully registered for Premium</h1>
+	</body>
+</html>
